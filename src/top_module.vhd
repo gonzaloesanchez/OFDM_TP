eb09d2bac40fafe7d4253b1b0148436743f4bff7
El_Gonza <gonzalo.e.sanchez@hotmail.com>
2018-12-23 18:05:08 -0300

    Agregados IPcores Encoder y Viterbi. Conexionado hecho

--- src/top_module.vhd
+++ src/top_module.vhd
@@ -40,7 +40,7 @@ architecture top_module_arch of top_module is
 
 	--DECLARACION DE COMPONENTES
 
-	--FIFO 16 BYTES 8 A 1
+	--FIFO IN 16 BYTES 8 A 1
 
 	COMPONENT fifo_16_8a1
 		PORT(
@@ -57,7 +57,34 @@ architecture top_module_arch of top_module is
 		);
 	END COMPONENT;
 
-	--FIFO 16 BYTES 1 A 8
+	--CODIFICADOR CONVOLUCIONAL
+
+	component encoder
+		port(
+			data_in    : in  std_logic;
+			data_out_v : out std_logic_vector(1 downto 0);
+			rdy        : out std_logic;
+			nd			  : in std_logic;
+			ce         : in  std_logic;
+			sclr       : in  std_logic;
+			clk        : in  std_logic);
+	end component;
+
+	--DECODIFICADOR VITERBI
+
+	component viterbi
+		port(
+			data_in0 : in  std_logic_vector(0 downto 0);
+			data_in1 : in  std_logic_vector(0 downto 0);
+			data_out : out std_logic;
+			rdy      : out std_logic;
+			ce       : in  std_logic;
+			sclr     : in  std_logic;
+			clk      : in  std_logic);
+	end component;
+
+
+	--FIFO OUT 16 BYTES 1 A 8
 
 	COMPONENT fifo_16_1a8
 		PORT(
@@ -79,14 +106,25 @@ architecture top_module_arch of top_module is
 	--GENERALES
 	signal clk_s,clk2x_s, rst_s : std_logic;
 
-	--FIFO ENTRADA -> FIFO SALIDA
-	signal fifo_in_rd_en_s,fifo_in_empty_s, fifo_out_wr_en_s : std_logic;
+	--FIFO ENTRADA
+	signal fifo_in_data_out : std_logic_vector(0 downto 0);
+	signal fifo_in_rd_en_s,fifo_in_empty_s : std_logic;
+	signal fifo_in_do_v : std_logic;
+
+	--ENCODER
+	signal encoder_data_in : std_logic;
+	signal encoder_data_out : std_logic_vector(1 downto 0);
+	signal encoder_do_v,encoder_new_data_in : std_logic;
+
+ --VITERBI
+	signal viterbi_data_out,viterbi_data_in0,viterbi_data_in1: std_logic_vector(0 downto 0);
+	signal viterbi_rdy_s,viterbi_ce : std_logic;
+
+	--FIFO SALIDA
+	signal fifo_out_empty_s, fifo_out_rd_en_s, fifo_out_wr_en_s : std_logic;
 	signal fifo_out_data_in_s: std_logic_vector(0 downto 0);
 
 
- --VITERBI -> FIFO SALIDA
---	signal decoder_out_s: std_logic_vector(0 downto 0);
-	signal fifo_out_empty_s, fifo_out_rd_en_s : std_logic;
 
 begin
 
@@ -105,14 +143,52 @@ begin
 			din    => h2fData_out,
 			wr_en  => h2fValid_out,
 			rd_en  => fifo_in_rd_en_s,
-			dout   => fifo_out_data_in_s,
+			dout   => fifo_in_data_out,
 			full   => open,
 			empty  => fifo_in_empty_s,
-			valid  => fifo_out_wr_en_s
+			valid  => fifo_in_do_v
 		);
 
 		fifo_in_rd_en_s<=not fifo_in_empty_s;
 
+		--Conexiones desde FIFO IN a ENCODER
+		encoder_data_in <= fifo_in_data_out;
+		encoder_new_data_in <= fifo_in_do_v;
+
+
+		--Encoder
+		encoder0 : encoder
+		PORT MAP(
+			data_in    => encoder_data_in,
+			data_out_v => encoder_data_out,
+			rdy        => encoder_do_v,
+			nd 				 => encoder_new_data_in,
+			ce         => '1',
+			sclr       => rst_s,
+			clk        => clk_s
+		);
+
+		--Conexiones desde ENCODER a VITERBI
+		viterbi_data_in0(0) <= encoder_data_out(0);
+		viterbi_data_in1(0) <= encoder_data_out(1);
+		viterbi_ce <= encoder_do_v;
+
+
+		--DECODIFICADOR VITERBI
+	viterbi0 : viterbi
+		PORT MAP(
+			data_in0 => viterbi_data_in0(0),
+			data_in1 => viterbi_data_in1(0),
+			data_out => viterbi_data_out(0),
+			rdy      => viterbi_rdy_s,
+			ce       => viterbi_ce,
+			sclr     => rst_s,
+			clk      => clk_s
+		);
+
+--Conexiones desde VITERBI a FIFO OUT
+	fifo_out_data_in_s <= viterbi_data_out(0);
+	fifo_out_wr_en_s <= viterbi_rdy_s;
 
 		--FIFO DE SALIDA
 	fifo_out0 : fifo_16_1a8
